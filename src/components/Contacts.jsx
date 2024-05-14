ea854080ddc335eb12419d1a8224208c5af1b45c
Sardar Balnour <balnour.sardar@gmail.com>
2024-05-14 17:53:03 +0330

    contact component and form

---
+++ src/components/Contacts.jsx
@@ -0,0 +1,60 @@
+import { useState } from "react";
+
+function Contacts() {
+  const [contact, setContact] = useState({
+    name: "",
+    lastName: "",
+    email: "",
+    phone: "",
+  });
+
+  const changeHandler = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+
+    setContact((contact) => ({ ...contact, [name]: value }));
+  };
+
+  const addHandler = () => {
+    console.log(contact);
+  };
+
+  return (
+    <div>
+      <div>
+        <input
+          name="name"
+          //   in name ha bayad ba maqadir mojod dar state yni klid ha dqiqn yki bashe
+          type="text"
+          placeholder="Name"
+          value={contact.name}
+          onChange={changeHandler}
+        />
+        <input
+          name="lastName"
+          type="text"
+          placeholder="Last Name"
+          value={contact.lastName}
+          onChange={changeHandler}
+        />
+        <input
+          name="email"
+          type="email"
+          placeholder="Email"
+          value={contact.email}
+          onChange={changeHandler}
+        />
+        <input
+          name="phone"
+          type="number"
+          placeholder="Phone"
+          value={contact.phone}
+          onChange={changeHandler}
+        />
+        <button onClick={addHandler}>Add Contact</button>
+      </div>
+    </div>
+  );
+}
+
+export default Contacts;
